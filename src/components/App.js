1d27ca3ac6cac79b2d87528a772ecc1f4e817348
Wasef Hussain <12345678wasef@gmail.com>
2021-03-14 01:27:18 +0500

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Color from '../abis/Color.json'
+
 import './App.css';
 import Web3 from 'web3'
 class App extends Component {
@@ -28,25 +29,46 @@ class App extends Component {
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
 
-    // const networkId = await web3.eth.net.getId()
-    // const networkData = Color.networks[networkId]
-    // if(networkData) {
-    //   const abi = Color.abi
-    //   const address = networkData.address
-    //   const contract = new web3.eth.Contract(abi, address)
-    //   this.setState({ contract })
-    //   const totalSupply = await contract.methods.totalSupply().call()
-    //   this.setState({ totalSupply })
+    const networkId = await web3.eth.net.getId()
+    const networkData = Color.networks[networkId]
+    if(networkData) {
+      const abi = Color.abi
+      const address = networkData.address
+      const contract = new web3.eth.Contract(abi, address)
+      // console.log(contract)
+      this.setState({ contract })
+      const totalSupply = await contract.methods.totalSupply().call()
+      this.setState({ totalSupply })
     //   // Load Colors
-    //   for (var i = 1; i <= totalSupply; i++) {
-    //     const color = await contract.methods.colors(i - 1).call()
-    //     this.setState({
-    //       colors: [...this.state.colors, color]
-    //     })
-    //   }
-    // } else {
-    //   window.alert('Smart contract not deployed to detected network.')
-    // }
+      for (var i = 1; i <= totalSupply; i++) {
+        const color = await contract.methods.colors(i - 1).call()
+        this.setState({
+          colors: [...this.state.colors, color]
+        })
+      }
+      console.log(this.state.colors)
+    } else {
+      window.alert('Smart contract not deployed to detected network.')
+    }
+  }
+  mint = (color) => {
+    this.state.contract.methods.mint(color).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({
+        colors: [...this.state.colors, color]
+      })
+    })
+  }
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      contract: null,
+      totalSupply: 0,
+      colors: []
+    }
   }
 
 
@@ -62,32 +84,48 @@ class App extends Component {
           >
             Color Tokens
           </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white"><span id="account">{this.state.account}</span></small>
+            </li>
+          </ul>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+              <h1>Issue Token</h1>
+                <form onSubmit={(event) => {
+                  event.preventDefault()
+                  const color = this.color.value
+                  this.mint(color)
+                }}>
+                  <input
+                    type='text'
+                    className='form-control mb-1'
+                    placeholder='e.g. #FFFFFF'
+                    ref={(input) => { this.color = input }}
+                  />
+                  <input
+                    type='submit'
+                    className='btn btn-block btn-primary'
+                    value='MINT'
+                  />
+                </form>
+            
               </div>
             </main>
+          </div>
+          <hr/>
+          <div className="row text-center">
+          {this.state.colors.map((color,key)=>{
+            return(
+                <div key={key} className="col-md-3 mb-3">
+                  <div className="token" style={{ backgroundColor: color }}></div>
+                  <div>{color}</div>
+                </div>
+              )
+          })}
           </div>
         </div>
       </div>
